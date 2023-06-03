a6950aa2b23a02511590cae5f62267d97eb32a1d
Gzh0821 <gaozih0821@outlook.com>
2023-06-03 18:53:01 +0800

    fix bug to defend xss inject.

--- webroot/cgi-bin/calculator.py
+++ webroot/cgi-bin/calculator.py
@@ -9,14 +9,14 @@ class CalculatorCgi(ArgCgi):
             first = float(self['first'])
             second = float(self['second'])
             rule = self['rule']
-            res = 'Error!'
+            res = 'Wrong Operator!'
             if rule == 'add':
                 res = first + second
             elif rule == 'multi':
                 res = first * second
             self.add_output('result', str(res))
         except Exception as e:
-            self.add_output('result', str(e))
+            self.add_output('result', 'Wrong Input!')
         self.add_output('tool', 'result by python')
 
 
